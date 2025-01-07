dd3613ea3b6f7f3f97d044ee000b8b014308f89b
fmrg03 <froilanmartinr@gmail.com>
2025-01-07 14:00:39 -0300

    Mod | user service to testing

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { UpdateUserDto } from '../dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
   create(createUserDto: CreateUserDto) {
+    console.log('createUserDto', createUserDto);
     return 'This action adds a new user';
   }
 
